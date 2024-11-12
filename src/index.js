d4ebeff2cdce451484f0b348f506352b1bbd56d1
stephspace <swiafe.dev@gmail.com>
2024-11-12 10:29:47 -0500

    Implement sliding functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import "./styles.css";
+let leftArrow = document.querySelector('.left-arrow');
+let rightArrow = document.querySelector('.right-arrow');
+let slides = document.querySelectorAll('img');
+let firstSlide = 0;
+let lastSlide =  slides.length - 1;
+let activeSlide = 0;
 
+function prev(){
+    leftArrow.addEventListener('click', () => {
+        slides[activeSlide].classList.add('visibility');
+
+        if(activeSlide != firstSlide){
+            activeSlide--;
+        } else {
+            activeSlide = lastSlide;
+        }
+        slides[activeSlide].classList.remove('visibility');
+    });
+}
+
+function next(){
+    rightArrow.addEventListener('click', () => {
+        slides[activeSlide].classList.add('visibility');
+
+        if(activeSlide != lastSlide){
+            activeSlide++;
+        } else {
+            activeSlide = firstSlide;
+        }
+        slides[activeSlide].classList.remove('visibility');
+    });
+}
+
+prev();
+next();
 
